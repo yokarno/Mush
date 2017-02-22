30a12ea39b82322b33934aaf781e4830f48fbb38
Zhong Yokarno <spraydew@sina.com>
2017-02-22 22:19:28 +0800

    Add files via upload

---
+++ lua/hy/common/cmd.lua
@@ -0,0 +1,67 @@
+require("wait")
+require("hy.common.pathtbl")
+
+module (..., package.seeall)
+
+
+function SendToWorld(str)
+	wait.make(function()
+		send_cmd(str)
+	end)
+end
+
+
+function send_cmd(str)
+	if str == nil then 
+		return
+	end
+
+	local path = Paths[str]
+	if path == nil then
+		path = str
+	end
+
+	local temp_tbl = utils.split(path,";")
+	for k,v in pairs(temp_tbl) do	
+	  	if Paths[v] == nil then
+		  	send_cmd_to_world(v)
+		else
+			local tmpPaths = utils.split(Paths[v],";")
+			local tmpdeadloopflag = false
+			for k1,v1 in pairs(tmpPaths) do
+				if v1 == v then
+					tmpdeadloopflag = true
+				end
+			end
+			if tmpdeadloopflag == false then
+				send_cmd(Paths[v])
+			else
+				send_cmd_to_world(Paths[v])
+			end
+	  	end
+	end		
+end
+
+function send_cmd_to_world(cmd)
+	local s,e,reg = string.find (cmd, "^#(%d+) ")
+	if reg == nil then
+		world.Execute(cmd)
+		return
+	else
+		local times = tonumber(string.sub(cmd,2,e))
+		if times == 0 then 
+			world.Execute(cmd)
+			return
+		end
+	
+		local str = string.sub(cmd,e+1,string.len(cmd))
+		--expanded repeat cmd
+		--wait.time(world.SpeedWalkDelay)
+		for i =1,times do
+			world.Execute(str)
+			wait.time(world.SpeedWalkDelay)
+		end
+		return
+	end 
+end
+
