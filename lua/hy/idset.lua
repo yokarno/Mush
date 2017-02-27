ced3eac2250f3dbf72683dfdcb2520cc2c088c9e
yokarno <spraydew@sina.com>
2017-02-27 16:21:40 +0800

    modify idset.lua

--- lua/hy/idset.lua
+++ lua/hy/idset.lua
@@ -1,7 +1,13 @@
 --module (..., package.seeall)
 
-FullSkill = 1
+FullSkill = 0  --flags id need to full skill
 
-AutoResearch = 0
+AutoResearch = 0  -- flags id will auto research skill 
 
-FullSkillInJob = 0
+FullSkillInJob = 0   -- flags id will full skill in job internal 
+
+Newbie = 1  -- flags is newbie login
+
+JieduId = 1  -- flags jiedu id
+
+ChatId = 0  -- flags chatid
